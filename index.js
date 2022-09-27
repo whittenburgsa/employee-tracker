39251d16b44609ca9dbe9680cd72077088fb35a4
whittenburgsa <whittenburgsavannah@gmail.com>
2022-09-27 19:06:01 -0400

    added more async functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const utils = require("util");
+const { async } = require("rxjs");
 //require ("console.table")
 
 var connection = mysql.createConnection({
@@ -17,7 +18,7 @@ function prompts() {
             type: "list",
             name: "choice",
             message: "What would you like to do?",
-            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Update an employee role", "Quit"]
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Quit"]
 
 
         }
@@ -29,16 +30,21 @@ function prompts() {
             viewAllRoles()
         }
         else if (answer.choice === "View all employees") {
-
+            viewAllEmployees()
         }
         else if (answer.choice === "Add a department") {
-
+            addDepartment()
         }
         else if (answer.choice === "Add a role") {
-
+            addRole()
         }
-        else if (answer.choice === "Update an employee role") {
 
+        else if (answer.choice === "Add an employee") {
+            addEmployee()
+        }
+
+        else if (answer.choice === "Update an employee role") {
+            updateEmployeeRole()
         }
         else {
             process.exit()
@@ -52,9 +58,63 @@ async function viewAllDepartments() {
 }
 
 async function viewAllRoles() {
-    const roles = await connection.query("Select * from role")
+    const roles = await connection.query("Select role.id, role.title, role.salary, department.name as department_name from role left join department on role.department_id = department.id")
     console.table(roles)
     prompts()
+}
+
+async function viewAllEmployees() {
+    const employees = await connection.query("Select employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name as department_name, concat(manager.first_name, ' ', manager.last_name) as manager from employee left join role on employee.role_id = role.id left join department on role.department_id = department.id left join employee manager on manager.id = employee.manager_id")
+    console.table(employees)
+    prompts()
+}
+
+async function addDepartment() {
+    const answer = await inquirer.prompt([{
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter name of department'
+    }])
+    await connection.query("Insert into department (name) values (?)", [answer.departmentName])
+    console.log("Your department was successfully added")
+    prompts()
+}
+
+async function addEmployee() {
+    const roles = await connection.query("Select * from employees")
+    console.table(employees)
+    prompts()
+}
+
+async function addRole() {
+    const department = await connection.query ("Select * from department")
+    const departmentChoices = department.map(department => ({
+        value: department.id, name: department.name
+    }))
+    const answer = await inquirer.prompt([{
+        type: 'input',
+        name: 'roleName',
+        message: 'Enter name of role'
+    }, {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary for the role?'
+    }, {
+        type: 'list',
+        name: 'departmentId',
+        message: 'What is the department?',
+        choices: departmentChoices
+    }])
+    await connection.query("Insert into role (title, salary, department_id) values (?, ?, ?)", [answer.roleName, answer.salary, answer.departmentId])
+    console.log ("Your role was successfully added")
+    prompts()
+
+
+    async function updateEmployeeRole() {
+        const roles = await connection.query("Select * from employees")
+        console.table(employees)
+        prompts()
+    }
 }
 
 prompts()
